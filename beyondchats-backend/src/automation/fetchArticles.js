8f003319ec4383eeff7ecfcd7af065dd5ec4ed29
udayasri-pagilla <udayasripagilla1873@gmail.com>
2025-12-31 11:09:40 +0530

    publishArticle updated

--- beyondchats-backend/src/automation/fetchArticles.js
+++ beyondchats-backend/src/automation/fetchArticles.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api/articles";
+const API_BASE_URL = "https://beyondchats-backend-ys8y.onrender.com/api/articles";
 
 const fetchArticles = async () => {
   try {
